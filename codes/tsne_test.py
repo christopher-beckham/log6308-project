8c3769fd52be70190c9f403b74b3a8353afabf5a
Christopher Beckham <chrispy645@gmail.com>
2016-11-14 10:11:56 -0500

    add prelim tsne code

---
+++ codes/tsne_test.py
@@ -0,0 +1,15 @@
+import sys
+
+import numpy as np
+from sklearn.manifold import TSNE
+#import matplotlib.pyplot as plt
+
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+dat = np.load(in_file)
+
+model = TSNE(n_components=2, random_state=0, verbose=2)
+np.set_printoptions(suppress=True)
+transformed = model.fit_transform(dat)
+
+np.save(out_file, transformed)
